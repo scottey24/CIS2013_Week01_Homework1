dcab5fafa99908799471252ac8604b7db0f85047
scottey24 <46961419+scottey24@users.noreply.github.com>
2019-01-30 04:51:32 -0600

    Add files via upload

---
+++ CIS2013_Week01_Homework1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+	int main(){
+	    cout<<"*******************************************\n";
+		cout<<"Scott Culp\n";
+		cout<<"2112 Oakside Dr" <<"		P: (580)699-6219\n";
+		cout<<"Norman, Ok 73071" <<"	E: scott.r.culp@okstate.edu\n";
+		cout<<"							G: github.com/scottey24
+		cout<<"********************************************\n";
+		return 0;
+} 
