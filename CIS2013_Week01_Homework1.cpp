a44a718c98e6fa4bacfe39dfaa655927c89fc0e1
scottey24 <46961419+scottey24@users.noreply.github.com>
2019-01-30 17:08:50 -0600

    Update CIS2013_Week01_Homework1.cpp

--- CIS2013_Week01_Homework1.cpp
+++ CIS2013_Week01_Homework1.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 	int main(){
-	    cout<<"*******************************************\n";
+	    cout<<"********************************************************************\n";
 		cout<<"Scott Culp\n";
 		cout<<"2112 Oakside Dr" <<"		P: (580)699-6219\n";
-		cout<<"Norman, Ok 73071" <<"	E: scott.r.culp@okstate.edu\n";
-		cout<<"							G: github.com/scottey24
-		cout<<"********************************************\n";
+		cout<<"Norman, Ok 73071" <<"		E: scott.r.culp@okstate.edu\n";
+		cout<<"					G: github.com/scottey24
+		cout<<"*****************************************************************\n";
 		return 0;
 } 
